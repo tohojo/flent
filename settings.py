1fb0c7a3df8aeffe7f7cb045405287035b2605df
Toke Høiland-Jørgensen <toke@toke.dk>
2012-11-25 23:33:48 +0100

    Add --list-tests and --list-plots options.

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, runpy, optparse
+import sys, os, runpy, optparse
 
 from ordereddict import OrderedDict
 
@@ -32,6 +32,8 @@ DEFAULT_SETTINGS = {
     'TITLE': '',
     'LOG_FILE': None,
     'INPUT': None,
+    'DESCRIPTION': 'No description',
+    'PLOTS': {},
     }
 
 TEST_PATH = os.path.join(os.path.dirname(__file__), 'tests')
@@ -46,6 +48,8 @@ parser.add_option("-i", "--input", action="store", type="string", dest="INPUT",
                   help="file to read input from (instead of running tests)")
 parser.add_option("-f", "--format", action="store", type="string", dest="FORMAT",
                   help="override config file output format")
+parser.add_option("-p", "--plot", action="store", type="string", dest="PLOT",
+                  help="select which plot to output for the given test")
 parser.add_option("-H", "--host", action="store", type="string", dest="HOST",
                   help="host to connect to for tests")
 parser.add_option("-t", "--title-extra", action="store", type="string", dest="TITLE",
@@ -56,6 +60,11 @@ parser.add_option("-L", "--length", action="store", type="int", dest="LENGTH",
                   help="base test length (some tests may add some time to this)")
 parser.add_option("-s", "--step-size", action="store", type="float", dest="STEP_SIZE",
                   help="measurement data point step size")
+
+parser.add_option('--list-tests', action='store_true', dest="LIST_TESTS",
+                  help="list available tests")
+parser.add_option('--list-plots', action='store_true', dest="LIST_PLOTS",
+                  help="list available tests")
 
 class Settings(optparse.Values, object):
 
@@ -69,6 +78,11 @@ class Settings(optparse.Values, object):
             if k == k.upper():
                 setattr(self, k, v)
 
+        if 'DEFAULTS' in settings:
+            for k,v in settings['DEFAULTS'].items():
+                if not hasattr(self, k):
+                    setattr(self, k, v)
+
         if not 'TOTAL_LENGTH' in settings:
             self.TOTAL_LENGTH = self.LENGTH
 
@@ -77,10 +91,18 @@ class Settings(optparse.Values, object):
             v = OrderedDict(v)
         object.__setattr__(self, k, v)
 
+    def update(self, values):
+        for k,v in values.items():
+            setattr(self, k, v)
+
 settings = Settings(DEFAULT_SETTINGS)
+
 
 def load():
     (dummy,args) = parser.parse_args(values=settings)
+
+    if hasattr(settings, 'LIST_TESTS') and settings.LIST_TESTS:
+        list_tests()
 
     if len(args) < 1:
         parser.error("Missing test name.")
@@ -92,4 +114,29 @@ def load():
 
     settings.load_test(test_name)
 
+    if hasattr(settings, 'LIST_PLOTS') and settings.LIST_PLOTS:
+        list_plots()
+
     return settings
+
+def list_tests():
+    tests = sorted([os.path.splitext(i)[0] for i in os.listdir(TEST_PATH) if i.endswith('.conf')])
+    sys.stderr.write('Available tests:\n')
+    max_len = unicode(max([len(t) for t in tests]))
+    for t in tests:
+        settings.update(DEFAULT_SETTINGS)
+        settings.load_test(t)
+        sys.stderr.write((u"  %-"+max_len+u"s :  %s\n") % (t, settings.DESCRIPTION))
+    sys.exit(0)
+
+def list_plots():
+    plots = settings.PLOTS.keys()
+    if not plots:
+        sys.stderr.write("No plots available for test '%s'.\n" % settings.NAME)
+        sys.exit(0)
+
+    sys.stderr.write("Available plots for test '%s':\n" % settings.NAME)
+    max_len = unicode(max([len(p) for p in plots]))
+    for p in plots:
+        sys.stderr.write((u"  %-"+max_len+u"s :  %s\n") % (p, settings.PLOTS[p]['description']))
+    sys.exit(0)
