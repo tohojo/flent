017c54b173251cbd6ad476b6843a57d6ffaf9797
Toke Høiland-Jørgensen <toke@toke.dk>
2012-11-29 16:32:20 +0100

    Hopefully handle missing data a bit better.

--- aggregators.py
+++ aggregators.py
@@ -153,15 +153,17 @@ class TimeseriesAggregator(Aggregator):
 
     def aggregate(self, results):
         measurements = self.collect()
+        if not measurements:
+            raise RuntimeError(u"No data to aggregate. Run with -l and check log file to investigate.")
         results.create_series(measurements.keys())
 
         # We start steps at the minimum time value, and do as many steps as are
         # necessary to get past the maximum time value with the selected step
         # size
-        first_times = [i[0][0] for i in measurements.values() if i[0]]
-        last_times = [i[-1][0] for i in measurements.values() if i[-1]]
+        first_times = [i[0][0] for i in measurements.values() if i and i[0]]
+        last_times = [i[-1][0] for i in measurements.values() if i and i[-1]]
         if not (first_times or last_times):
-            raise RuntimeError(u"No data to aggregate")
+            raise RuntimeError(u"No data to aggregate. Run with -l and check log file to investigate.")
         t_0 = min(first_times)
         t_max = max(last_times)
         steps = int(math.ceil((t_max-t_0)/self.step))
