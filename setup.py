db39eb14a547fc70835d3e1793ba50cabedbdd13
Toke Høiland-Jørgensen <toke@toke.dk>
2013-03-27 11:02:17 +0100

    Rework setup.py script to add a --fake-root parameter for use when packaging, to ignore the --root to the install command when creating packages.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@
 import sys, os
 from distutils.core import setup
 from distutils.command.build_py import build_py as _build_py
+from distutils.command.install import install as _install
 from netperf_wrapper.build_info import VERSION
 from glob import glob
 
@@ -31,6 +32,16 @@ if sys.version_info[:2] < (2,6):
     sys.stderr.write("Sorry, netperf-wrapper requires v2.6 or later of Python\n")
     sys.exit(1)
 
+class install(_install):
+    user_options = _install.user_options + [('fake-root', None,
+                                              'indicates that --root is fake'
+                                              ' (e.g. when creating packages.)')]
+    boolean_options = _install.boolean_options + ['fake-root']
+
+    def initialize_options(self):
+        _install.initialize_options(self)
+        self.fake_root = False
+
 class build_py(_build_py):
     """build_py command
 
@@ -38,6 +49,7 @@ class build_py(_build_py):
     'netperf_wrapper.build_config' so that it contains information on
     installation prefixes afterwards.
     """
+
     def build_module (self, module, module_file, package):
         orig_content = None
         if ( module == 'build_info' and package == 'netperf_wrapper'
@@ -46,11 +58,17 @@ class build_py(_build_py):
             with open(module_file, 'r') as module_fp:
                 orig_content = module_fp.read()
 
+            if iobj.fake_root:
+                prefix = iobj.prefix
+                print(prefix)
+            else:
+                prefix = iobj.install_data
+
             with open(module_file, 'w') as module_fp:
                 module_fp.write('# -*- coding: UTF-8 -*-\n\n')
                 module_fp.write("VERSION='%s'\n"%(version_string))
                 module_fp.write("DATA_DIR='%s'\n"%(
-                    os.path.join(iobj.install_data, 'share', 'netperf-wrapper')))
+                    os.path.join(prefix, 'share', 'netperf-wrapper')))
 
         _build_py.build_module(self, module, module_file, package)
 
@@ -83,5 +101,5 @@ setup(name="netperf-wrapper",
       packages = ["netperf_wrapper"],
       scripts = ["netperf-wrapper"],
       data_files = data_files,
-      cmdclass = {'build_py': build_py},
+      cmdclass = {'build_py': build_py, 'install': install},
     )
