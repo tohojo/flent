22b52fed38cf1fae67c811778833a9f7ced18957
Toke Høiland-Jørgensen <toke@toke.dk>
2014-05-05 00:29:15 +0200

    Make -b ALL run all configured batch tests.

--- netperf_wrapper/batch.py
+++ netperf_wrapper/batch.py
@@ -321,7 +321,11 @@ class BatchRunner(object):
         if self.settings.INPUT:
             return self.load_input(self.settings)
         elif self.settings.BATCH_NAMES:
-            for b in self.settings.BATCH_NAMES:
+            if len(settings.BATCH_NAMES) == 1 and settings.BATCH_NAMES[0] == 'ALL':
+                batches = self.batches.keys()
+            else:
+                batches = self.settings.BATCH_NAMES
+            for b in batches:
                 self.run_batch(b)
             return True
         else:
