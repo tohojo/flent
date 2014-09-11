643e1103ae1f6531bf5804a736d1cfc06a27d841
Toke Høiland-Jørgensen <toke@toke.dk>
2014-09-11 11:15:35 +0200

    Change colours to use the '8-class Dark2' scheme from colorbrewer.org.

--- netperf_wrapper/formatters.py
+++ netperf_wrapper/formatters.py
@@ -248,7 +248,7 @@ class PlotFormatter(Formatter):
 
     linestyles = ['-', '--', ':']
     markers = ['o', '^', 's', 'v', 'D', '*', '<', '>', 'x', '+']
-    colours = ['b', 'g', 'r', 'c', 'm', 'k', 'orange', 'grey', '#a52a2a', 'lime']
+    colours = ["#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#e6ab02", "#a6761d", "#666666"]
     dashes = [[8,4,2,4],
               ]
 
