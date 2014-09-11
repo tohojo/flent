153c7cabcb5633b7e5f91fb29ad4d4747f5182fc
Toke Høiland-Jørgensen <toke@toke.dk>
2014-09-11 16:49:25 +0200

    Update mkrelease.sh to also do renaming to -git versions.

--- mkrelease.sh
+++ mkrelease.sh
@@ -12,19 +12,22 @@ die()
 
 echo ==== Updating source code version numbers to ${VERSION}... ====
 
-sed -i s/VERSION=\"[0-9\\.]\\+-git\"/VERSION=\"${VERSION}\"/ netperf_wrapper/build_info.py  || die error
-sed -i -e "s/Netperf-wrapper v[0-9\\.]\\+-git/Netperf-wrapper v${VERSION}/" -e "1c .TH NETPERF-WRAPPER \"1\" \"$(date +"%B %Y")\" \"Netperf-wrapper v${VERSION}.\" \"User Commands\"" man/netperf-wrapper.1  || die error
+sed -i s/VERSION=\"[0-9\\.]\\+\\\(-git\\\)\\?\"/VERSION=\"${VERSION}\"/ netperf_wrapper/build_info.py  || die error
+sed -i -e "s/Netperf-wrapper v[0-9\\.]\\+\(-git\)\?/Netperf-wrapper v${VERSION}/" -e "1c .TH NETPERF-WRAPPER \"1\" \"$(date +"%B %Y")\" \"Netperf-wrapper v${VERSION}.\" \"User Commands\"" man/netperf-wrapper.1  || die error
 
-echo ==== Creating and signing release tarball... ====
-python setup.py sdist  || die error
-gpg --detach-sign dist/netperf-wrapper-${VERSION}.tar.gz  || die error
+if [[ ! "$VERSION" =~ -git$ ]]; then
 
-echo ==== Updating packaging files... ====
-SHA1=$(sha1sum dist/netperf-wrapper-${VERSION}.tar.gz | awk '{print $1}')
-sed -i -e "s/pkgver=.*/pkgver=${VERSION}/" -e "s/sha1sums=([^)]\\+)/sha1sums=('${SHA1}')/" packaging/archlinux/PKGBUILD  || die error
 
-tmpfile=$(mktemp)
-cat >$tmpfile <<EOF
+    echo ==== Creating and signing release tarball... ====
+    python setup.py sdist  || die error
+    gpg --detach-sign dist/netperf-wrapper-${VERSION}.tar.gz  || die error
+
+    echo ==== Updating packaging files... ====
+    SHA1=$(sha1sum dist/netperf-wrapper-${VERSION}.tar.gz | awk '{print $1}')
+    sed -i -e "s/pkgver=.*/pkgver=${VERSION}/" -e "s/sha1sums=([^)]\\+)/sha1sums=('${SHA1}')/" packaging/archlinux/PKGBUILD  || die error
+
+    tmpfile=$(mktemp)
+    cat >$tmpfile <<EOF
 netperf-wrapper (${VERSION}-1) precise; urgency=low
 
   * Bump to release ${VERSION}.
@@ -33,8 +36,10 @@ netperf-wrapper (${VERSION}-1) precise; urgency=low
 
 EOF
 
-cat packaging/debian/changelog >> $tmpfile  || die error
-mv $tmpfile packaging/debian/changelog || die error
+    cat packaging/debian/changelog >> $tmpfile  || die error
+    mv $tmpfile packaging/debian/changelog || die error
+
+fi
 
 echo ==== Staging changed files ====
 git add netperf_wrapper/build_info.py man/netperf-wrapper.1 packaging/debian/changelog packaging/archlinux/PKGBUILD || die error
