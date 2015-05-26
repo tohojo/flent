a742dd9ff456d3f503f900501031565f1cdda720
Toke Høiland-Jørgensen <toke@toke.dk>
2015-05-26 23:05:47 +0200

    Unbreak running from the git checkout dir.

--- flent/build_info.py
+++ flent/build_info.py
@@ -25,7 +25,7 @@ import os
 
 # this value works for the source distribution
 VERSION="0.11.1-git"
-DATA_DIR=os.path.normpath(os.path.join(os.path.dirname(__file__), '..'))
+DATA_DIR=os.path.dirname(__file__)
 
 ENCODING = "UTF-8"
 try:
