65dde73bb9293035d4a4db819d231c537fbaa6e5
Toke Høiland-Jørgensen <toke@toke.dk>
2015-07-25 19:43:06 +0200

    Fix loading of test data files for plotting unittest.

--- unittests/test_plotters.py
+++ unittests/test_plotters.py
@@ -297,7 +297,7 @@ dirname = os.path.join(os.path.dirname(__file__), "test_data")
 output_formats = ['svg', 'pdf', 'png']
 plot_suite = unittest.TestSuite()
 for fname,fmt in itertools.product(os.listdir(dirname), output_formats):
-    plot_suite.addTest(TestPlotting(fname, fmt))
+    plot_suite.addTest(TestPlotting(os.path.join(dirname,fname), fmt))
 
 
 
