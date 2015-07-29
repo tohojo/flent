b943577624ef7e4835f2759a48be94f5a2501641
Toke Høiland-Jørgensen <toke@toke.dk>
2015-07-29 16:11:39 +0200

    mkrelease.sh: Update doc/ instead of man page.

--- mkrelease.sh
+++ mkrelease.sh
@@ -13,8 +13,8 @@ die()
 echo ==== Updating source code version numbers to ${VERSION}... ====
 
 sed -i s/VERSION=\"[0-9\\.]\\+\\\(-git\\\)\\?\"/VERSION=\"${VERSION}\"/ flent/build_info.py  || die error
-sed -i -e "s/Flent v[0-9\\.]\\+\(-git\)\?/Flent v${VERSION}/" -e "1c .TH FLENT \"1\" \"$(date +"%B %Y")\" \"Flent v${VERSION}.\" \"User Commands\"" man/flent.1  || die error
-man -l -Hcat man/flent.1 > man/flent.1.html || die error
+sed -i -e "s/release = '[0-9\\.]\\+'/release = '${VERSION}'/" doc/conf.py  || die error
+make man || die error
 
 if [[ ! "$VERSION" =~ -git$ ]]; then
 
@@ -45,7 +45,7 @@ EOF
 fi
 
 echo ==== Staging changed files ====
-git add flent/build_info.py man/flent.1{,.html} packaging/debian/changelog packaging/archlinux/PKGBUILD || die error
+git add flent/build_info.py man/flent.1 doc/conf.py packaging/debian/changelog packaging/archlinux/PKGBUILD || die error
 
 echo ==== Done. Review changes and commit \(and tag\). ====
 [[ ! "$VERSION" =~ -git$ ]] && echo ==== Upload with \`twine upload dist/flent-${VERSION}*\`. ====
