1435a7a70d5cf5787c22c86ba21061893c446796
Toke Høiland-Jørgensen <toke@toke.dk>
2015-07-29 18:05:41 +0200

    Release v0.12.2.

--- doc/conf.py
+++ doc/conf.py
@@ -57,7 +57,7 @@ author = 'Toke Høiland-Jørgensen'
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
-version = '0.12.0-git'
+version = '0.12.2'
 #
 # The short X.Y version.
 # The full version, including alpha/beta/rc tags.
--- flent/build_info.py
+++ flent/build_info.py
@@ -24,7 +24,7 @@
 import os
 
 # this value works for the source distribution
-VERSION="0.12.0-git"
+VERSION="0.12.2"
 DATA_DIR=os.path.dirname(__file__)
 
 ENCODING = "UTF-8"
