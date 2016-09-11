b5bcc97a21f16f5f799728650b0f498804bb14a3
Toke Høiland-Jørgensen <toke@toke.dk>
2016-09-11 17:59:22 +0200

    FairnessRunner: Protect against crashing on all-zero data points.

--- flent/runners.py
+++ flent/runners.py
@@ -1212,5 +1212,8 @@ class FairnessRunner(ComputingRunner):
     command = "Fairness (computed)"
     def compute(self,values):
         if not len(values):
-            return 0.0
-        return math.fsum(values)**2/(len(values)*math.fsum([x**2 for x in values]))
+            return None
+        valsum = math.fsum([x**2 for x in values])
+        if not valsum:
+            return None
+        return math.fsum(values)**2/(len(values)*valsum)
