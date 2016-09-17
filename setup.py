4d5a5b1e05a2df69a12cecc8b4bafa5e3b83bb56
Toke Høiland-Jørgensen <toke@toke.dk>
2016-09-17 21:25:07 +0200

    Add Python 3.5 to list of supported versions.
    Closes #73.

--- setup.py
+++ setup.py
@@ -104,6 +104,7 @@ classifiers = [
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Internet',
     'Topic :: System :: Benchmark',
     'Topic :: System :: Networking',
