4530d452999213c68f21be7ea7859ddc870f751d
Toke Høiland-Jørgensen <toke@toke.dk>
2016-10-10 15:23:27 +0200

    Update copyright headers in Python files outside flent/ dir.

--- unittests/test_plotters.py
+++ unittests/test_plotters.py
@@ -1,23 +1,23 @@
-## -*- coding: utf-8 -*-
-##
-## test_plotters.py
-##
-## Author:   Toke Høiland-Jørgensen (toke@toke.dk)
-## Date:     24 July 2015
-## Copyright (c) 2015, Toke Høiland-Jørgensen
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+#
+# test_plotters.py
+#
+# Author:   Toke Høiland-Jørgensen (toke@toke.dk)
+# Date:     24 July 2015
+# Copyright (c) 2015-2016, Toke Høiland-Jørgensen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
