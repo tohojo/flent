b08e4f6e6e61789cc7a3089492f46a5dd698c3ea
Toke Høiland-Jørgensen <toke@toke.dk>
2016-10-10 13:10:28 +0200

    Change copyright header to single-# block comment.
    Makes the linter happy. Update copyright years while we're at it.

--- flent/build_info.py
+++ flent/build_info.py
@@ -1,23 +1,23 @@
-## -*- coding: utf-8 -*-
-##
-## build_info.py
-##
-## Author:   Toke Høiland-Jørgensen (toke@toke.dk)
-## Date:      6 December 2012
-## Copyright (c) 2012-2015, Toke Høiland-Jørgensen
-##
-## This program is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# -*- coding: utf-8 -*-
+#
+# build_info.py
+#
+# Author:   Toke Høiland-Jørgensen (toke@toke.dk)
+# Date:      6 December 2012
+# Copyright (c) 2012-2016, Toke Høiland-Jørgensen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # setup.py rewrites this file with the install prefix info
 
