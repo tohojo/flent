910b5414a8c9f2024faceccde0fe3c549b3d42ac
Toke Høiland-Jørgensen <toke@toke.dk>
2017-01-12 11:04:32 +0100

    setup.py: __future__ unicode_literals breaks setuptools on python2.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
 
 import os
 import sys
