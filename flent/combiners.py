cbeb7686c2910f68481542bf467fae119233269f
Toke Høiland-Jørgensen <toke@toke.dk>
2017-11-20 12:51:17 +0100

    combiners: Fix len check in group_by=both combiner
    Signed-off-by: Toke Høiland-Jørgensen <toke@toke.dk>

--- flent/combiners.py
+++ flent/combiners.py
@@ -344,7 +344,7 @@ class BothCombiner(Combiner):
     # second the series. Currently only works if there's just one series
     # name configured in the plot config.
     def group(self, groups, config):
-        if len(config['series']) >= 1:
+        if len(config['series']) > 1:
             raise RuntimeError("Cannot use group_by=both for plots with more "
                                "than one data series")
         series_names = []
